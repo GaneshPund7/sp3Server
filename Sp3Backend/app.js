80e4a53c288e78d3de4ca475c5e0e32a31e00a02
Ganesh <ganeshpund0000@gmail.com>
2024-01-28 18:15:06 +0530

    second cmt

--- Sp3Backend/app.js
+++ Sp3Backend/app.js
@@ -14,7 +14,8 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI,  useNewUrlParser= true,
+  useUnifiedTopology = true);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
